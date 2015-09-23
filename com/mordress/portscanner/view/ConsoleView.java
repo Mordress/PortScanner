3e165eb685f2360b1112081dc2d7576a0de0dd20
Mordress <Mordress@gmail.com>
2015-09-24 02:33:56 +0300

    optimize view

--- com/mordress/portscanner/view/ConsoleView.java
+++ com/mordress/portscanner/view/ConsoleView.java
@@ -10,52 +10,38 @@ import java.util.Scanner;
 
 public class ConsoleView {
 
-    static String ip;
-
-    static ArrayList<Integer> ports;
-
-    static boolean exit = false;
-
     public static void main(String[] args) {
+        boolean exit = false;
         while (!exit) {
             System.out.print("Enter host's IP: ");
             Scanner scanner = new Scanner(System.in);
-
+            String userInputHostIp;
             while (true) {
-                String input = scanner.nextLine();
-                ports = new ArrayList<>();
-
-                if (!ValidateIPV4.isValidIPV4(input)) {
+                userInputHostIp = scanner.nextLine();
+                if (userInputHostIp == null || !ValidateIPV4.isValidIPV4(userInputHostIp)) {
                     System.err.println("Wrong ip! Try again.");
                 } else {
-                    ip = input;
                     break;
                 }
             }
 
             System.out.println("Enter host's ports 1-65535 (using whitespace for split): ");
-            String input = scanner.nextLine();
-            String[] inputPorts = input.split("\\s");
+            String ports = scanner.nextLine();
+            String[] userInputPorts = ports.split("\\s");
 
-
-            for (String iter : inputPorts) {
+            for (String iter : userInputPorts) {
                 if (ValidatePorts.isValidPort(iter)) {
-                    ports.add(Integer.parseInt(iter));
+                    Host host = new Host(userInputHostIp, Integer.parseInt(iter));
+                    if (host.isOpen()) {
+                        System.out.println(host + " \t\tis opened");
+                    } else {
+                        System.out.println(host + " \t\tis closed");
+                    }
                 } else {
                     System.out.println(iter + "\tis not valid port number");
                 }
             }
 
-            if (ports != null) {
-                for (int prt : ports) {
-                    Host host = new Host(ip, prt);
-                    if (host.isOpen()) {
-                        System.out.println(host + " \tis opened");
-                    } else {
-                        System.out.println(host + " \tis closed");
-                    }
-                }
-            }
             System.out.println("\'rerun\' - for rerung programm, \'exit\' for exit");
             String userchoice = scanner.nextLine();
 
